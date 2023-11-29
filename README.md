57519ec8a55019779b83e0e0d6c9c81cc79a0f7c
mrProper-666 <mrproper.aka.dax@gmail.com>
2023-11-29 16:33:45 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# fblog-lens-ext README
+
+The fblog-lens-ext provides a fblog log viewer for Lens.
+
+## Features
+
+This plugin adds a menu item to the Pods menu in Lens.  When clicked, it will open a new terminal with the fblog log viewer.
+
+## Requirements
+
+Install fblog globally
+
+## Installation Instructions
+
+Start the Lens is running, and follow these simple steps:
+
+1. Go to Extensions view (Menu -> File -> Extensions)
+2. Enter the name of this extension, `@mrProper-666/fblog-lens-ext`
+3. Click on the Install button
+4. Make sure the extension is enabled (Lens → Extensions)
+
+You may or may not need to refresh (or re-open) Lens for the plugin to render
+the menu item. This may be necessary if Lens is already open on the Pods workload.
+
+## Release Notes
+
+Enjoy!
+
+### 0.0.1
+
+Initial release of the fblog-lens-ext contains minimal functionality and allows
+for the viewing of Logs through the use of the `fblog` command line tool.
---
+++ src/pod-logs.tsx
@@ -0,0 +1,117 @@
+import React from "react"
+import { Common, Renderer } from "@k8slens/extensions"
+
+const {
+  Component: { MenuItem, Icon, SubMenu, StatusBrick },
+  Navigation,
+} = Renderer
+
+type Pod = Renderer.K8sApi.Pod
+
+export class PodLogs {
+
+  /**
+   * Get the container name list by a list of pods.
+   *
+   * @param podList
+   * @returns a set, without duplicates
+   */
+  public static getContainersByPodList(pod: Pod): Set<string> {
+    const containerNameList: Set<string> = new Set()
+    const containers = pod.getContainers()
+    for (let j = 0; j < containers.length; j++) {
+      containerNameList.add(containers[j].name)
+    }
+
+    return containerNameList
+  }
+
+  /**
+   * Construct the menu voices.
+   *
+   * @param props
+   * @param containerNames
+   * @param resourceNs
+   * @param resourceName
+   * @param resourceTitle
+   * @returns the MenuItem to show in Lens
+   */
+  public static uiMenu(
+    props: any,
+    containerNames: Set<string>,
+    resourceNs: string,
+    resourceName: string,
+    resourceTitle: string
+  ) {
+    return (
+      <MenuItem
+        onClick={Common.Util.prevDefault(() =>
+          this.podLogs(resourceNs, resourceName, resourceTitle, Array.from(containerNames)?.slice(-1)[0])
+        )}
+      >
+        <Icon
+          material="subject"
+          interactive={props.toolbar}
+          tooltip="Logs w/fblog"
+        />
+        <span className="title">Logs w/fblog</span>
+        {
+          containerNames.size >= 1 && (
+            <>
+              <Icon material="keyboard_arrow_right" />
+              <SubMenu>
+                {Array.from(containerNames).map((containerName) => {
+                  return (
+                    <MenuItem
+                      key={`only_${containerName}`}
+                      onClick={Common.Util.prevDefault(() =>
+                        this.podLogs(
+                          resourceNs,
+                          resourceName,
+                          resourceTitle,
+                          containerName
+                        )
+                      )}
+                    >
+                      <StatusBrick />
+                      <span>{containerName}</span>
+                    </MenuItem>
+                  )
+                })}
+              </SubMenu>
+            </>
+          )
+        }
+      </MenuItem>
+    )
+  }
+
+  private static podLogs(
+    resourceNs: string,
+    resourceName: string,
+    resourceTitle: string,
+    containerName?: string
+  ) {
+    // Generate log command with bunyan
+    const cmd = `kubectl logs -f -n ${resourceNs} ${resourceName} -c ${containerName} --tail=300 | fblog`
+
+    // Open new terminal
+    this.openTerminal(
+      `${resourceTitle}: ${resourceName}:${containerName} | fblog`,
+      cmd
+    )
+  }
+
+  private static openTerminal(title: string, command: string) {
+    const tab = Renderer.Component.createTerminalTab({
+      title: title,
+    })
+
+    Renderer.Component.terminalStore.sendCommand(command, {
+      enter: true,
+      tabId: tab.id,
+    })
+
+    Renderer.Navigation.hideDetails()
+  }
+}
